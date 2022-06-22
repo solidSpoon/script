30c047c16eca383b2ceec69aebf3a5c87620c57e
cedarrival <cedarrival@foxmail.com>
2022-06-22 14:57:31 +0800

    格式调整

--- src/main/java/org/example/script/PlayScriptCompiler.java
+++ src/main/java/org/example/script/PlayScriptCompiler.java
@@ -11,19 +11,15 @@ import org.example.script.antlr.PlayScriptParser;
  */
 public class PlayScriptCompiler {
 
-    AnnotatedTree at = null;
-    PlayScriptLexer lexer = null;
-    PlayScriptParser parser = null;
-
     public AnnotatedTree compile(String script, boolean verbose, boolean ast_dump) {
-        at = new AnnotatedTree();
+        AnnotatedTree at = new AnnotatedTree();
 
         //词法分析
-        lexer = new PlayScriptLexer(CharStreams.fromString(script));
+        PlayScriptLexer lexer = new PlayScriptLexer(CharStreams.fromString(script));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
         //语法分析
-        parser = new PlayScriptParser(tokens);
+        PlayScriptParser parser = new PlayScriptParser(tokens);
         at.ast = parser.prog();
 
         //语义分析
@@ -33,50 +29,50 @@ public class PlayScriptCompiler {
         //优点：1.代码更清晰；2.允许使用在声明之前，这在支持面向对象、递归函数等特征时是必须的。
         //pass1：类型和Scope
         TypeAndScopeScanner pass1 = new TypeAndScopeScanner(at);
-        walker.walk(pass1,at.ast);
+        walker.walk(pass1, at.ast);
 
         //pass2：把变量、类继承、函数声明的类型都解析出来。也就是所有声明时用到类型的地方。
         TypeResolver pass2 = new TypeResolver(at);
-        walker.walk(pass2,at.ast);
+        walker.walk(pass2, at.ast);
 
         //pass3：消解有的变量应用、函数引用。另外还做了类型的推断。
         RefResolver pass3 = new RefResolver(at);
-        walker.walk(pass3,at.ast);
+        walker.walk(pass3, at.ast);
 
         //pass4：类型检查
         TypeChecker pass4 = new TypeChecker(at);
-        walker.walk(pass4,at.ast);
+        walker.walk(pass4, at.ast);
 
         //pass5：其他语义检查
         SematicValidator pass5 = new SematicValidator(at);
-        walker.walk(pass5,at.ast);
+        walker.walk(pass5, at.ast);
 
         //pass6：做闭包的分析
         ClosureAnalyzer closureAnalyzer = new ClosureAnalyzer(at);
         closureAnalyzer.analyzeClosures();
 
         //打印AST
-        if (verbose || ast_dump){
-            dumpAST();
+        if (verbose || ast_dump) {
+            dumpAST(at, parser);
         }
 
         //打印符号表
-        if(verbose){
-            dumpSymbols();
+        if (verbose) {
+            dumpSymbols(at);
         }
 
         return at;
     }
 
     public AnnotatedTree compile(String script) {
-        return  compile(script,false, false);
+        return compile(script, false, false);
     }
 
     /**
      * 打印符号表
      */
-    public void dumpSymbols(){
-        if (at != null){
+    public void dumpSymbols(AnnotatedTree at) {
+        if (at != null) {
             System.out.println(at.getScopeTreeString());
         }
     }
@@ -84,8 +80,8 @@ public class PlayScriptCompiler {
     /**
      * 打印AST，以lisp格式
      */
-    public void dumpAST(){
-        if (at!=null) {
+    public void dumpAST(AnnotatedTree at, PlayScriptParser parser) {
+        if (at != null) {
             System.out.println(at.ast.toStringTree(parser));
         }
     }
@@ -93,9 +89,9 @@ public class PlayScriptCompiler {
     /**
      * 输出编译信息
      */
-    public void dumpCompilationLogs(){
-        if (at != null){
-            for (CompilationLog log : at.logs){
+    public void dumpCompilationLogs(AnnotatedTree at) {
+        if (at != null) {
+            for (CompilationLog log : at.logs) {
                 System.out.println(log);
             }
         }
